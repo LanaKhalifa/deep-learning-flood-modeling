72c6881184cd46e16cc508920352c64e1a768447
LanaKhalifa <102927321+LanaKhalifa@users.noreply.github.com>
2025-07-10 16:15:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from config import prj_sublists
-from database_generator import (
-    process_project,
-    concatenate_all_train_val_chunks,
-    concatenate_all_test_chunks,
-    concatenate_prj03_train_val_test_chunks,
-    generate_small_train_val_subset
-)
+from database_generator import (process_project,
+                                concatenate_all_train_val_chunks,
+                                concatenate_all_test_chunks,
+                                concatenate_prj03_train_val_test_chunks,
+                                generate_small_train_val_subset)
 
 print("Starting full flood patch generation pipeline...")
 
-# Step 1: Process each project and save patches in chunks
+# Step 1: Process each project and save patches in chunks of data due to memory limitation 
 for prj_num, (prj_name, sublists) in prj_sublists.items():
     process_project(prj_num, prj_name, sublists)
 
@@ -22,7 +20,7 @@ concatenate_all_test_chunks(prj_sublists)
 # Step 4: Concatenate project 03 train+val+test split separately
 concatenate_prj03_train_val_test_chunks()
 
-# Step 5: Create small subset for dev/debug purposes
+# Step 5: Create small subset
 generate_small_train_val_subset()
 
 print("All patch datasets have been generated and saved.")
