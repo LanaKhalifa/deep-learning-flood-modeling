0091a02d8dbe968b42662c497378027c8159c331
LanaKhalifa <102927321+LanaKhalifa@users.noreply.github.com>
2025-07-14 16:31:21 +0300

    Update generate_patches.py

--- simulations_to_samples/scripts/generate_patches.py
+++ simulations_to_samples/scripts/generate_patches.py
@@ -9,17 +9,15 @@ import pickle
 import numpy as np
 import logging
 from simulations_to_samples.scripts.patch_extractor_processor import PatchExtractorProcessor
-from config import prj_sublists, PATCHES_ROOT, DATABASES_ROOT, DATALOADERS_ROOT
+from config import PATCHES_ROOT
 
 logging.basicConfig(level=logging.INFO)
 
-def process_project(prj_num, prj_name, prj_list):
-    for idx, plan_num in enumerate(prj_list):
+def process_project(prj_num, prj_name, plans):
+    for plan_num in enumerate(plans):
         logging.info(f"Processing project {prj_num}")
         instance = PatchExtractorProcessor(prj_num, prj_name, plan_num)
         instance.generate_patches()  
-        save_dir = os.path.join( )
-        os.makedirs(save_dir, exist_ok=True)
 
         with open(os.path.join(PATCHES_ROOT, f'prj_{prj_num}_plan_{plan_num}_terrain_patches.pkl'), 'wb') as f:
             pickle.dump(instance.database['terrain'], f)
