d67173b22d40f9ea762432bdc9de14fa4cf82f74
LanaKhalifa <102927321+LanaKhalifa@users.noreply.github.com>
2025-07-16 01:00:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ print("=" * 60)
 print("🔷 Starting Flood Patch Generation... 🔷")
 print("=" * 60)
 
-
+"""
 # Step 1: Process each project to produce patches
 for prj_num, (prj_name, plans) in prjs_metadata.items():
     process_project(prj_num, prj_name, plans)
+"""
 
 print("=" * 60)
 print("🔷 Starting Datasets Generation... 🔷")
