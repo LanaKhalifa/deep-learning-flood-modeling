d2a08fdb18458be2427442ec713411c235112f20
LanaKhalifa <102927321+LanaKhalifa@users.noreply.github.com>
2025-07-15 22:25:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 from config import prjs_metadata
 from simulations_to_samples.scripts.generate_patches import process_project
+from simulations_to_samples.scripts.generate_datasets import create_and_save_datasets
 
 print("=" * 60)
-print("🔷 Starting Flood Patch Generation Pipeline... 🔷")
+print("🔷 Starting Flood Patch Generation... 🔷")
 print("=" * 60)
 
-# Step 1: Process each project
+"""
+# Step 1: Process each project to produce patches
 for prj_num, (prj_name, plans) in prjs_metadata.items():
     process_project(prj_num, prj_name, plans)
-
 """
-# Step 2: Concatenate train+val sets across all projects
-concatenate_all_train_val_chunks(prj_sublists)
 
-# Step 3: Concatenate test sets across all projects
-concatenate_all_test_chunks(prj_sublists)
+print("=" * 60)
+print("🔷 Starting Datasets Generation... 🔷")
+print("=" * 60)
 
-# Step 4: Concatenate project 03 train+val+test split separately
-concatenate_prj03_train_val_test_chunks()
+# Step 2: Create and save datasets
+create_and_save_datasets()
 
-# Step 5: Create small subset
-generate_small_train_val_subset()
 
-print("All patch datasets have been generated and saved.")
-"""
