24bef6a0f1d8302032cc879167037cd42bd916d3
LanaKhalifa <102927321+LanaKhalifa@users.noreply.github.com>
2025-07-16 15:11:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ create_and_save_datasets()
 print("=" * 60)
 print("🔷 Starting Dataloaders Generation... 🔷")
 print("=" * 60)
-
+"""
 # Step 3: Create and save dataloaders
 create_and_save_dataloaders()
-
+"""
 print("************************************************* STAGE 2: TRAIN DEEP LEARNING MODELS *************************************************")
 
 # ============================================
