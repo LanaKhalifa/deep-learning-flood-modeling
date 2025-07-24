010f3f234b134c8b2b8cc9c4881e7f6988f34ec5
LanaKhalifa <102927321+LanaKhalifa@users.noreply.github.com>
2025-07-24 11:40:53 +0300

    Delete Functions directory

--- Functions/save_trained_model.py
+++
@@ -1,4 +0,0 @@
-import torch
-
-def save_trained_model(netG, model_path):
-    torch.save(netG.state_dict(), model_path)
--- Functions/zero_internal.py
+++
@@ -1,7 +0,0 @@
-def zero_internal(matrix, boundary_thickness):
-    print('ttttttttttttttttttttttttttttt', matrix.shape)
-    rows, cols = matrix.shape
-    for i in range(boundary_thickness, rows - boundary_thickness):
-        for j in range(boundary_thickness, cols - boundary_thickness):
-            matrix[i][j] = 0
-    return matrix
