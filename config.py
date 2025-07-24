111b2121a68ba7e2ceb52731c1afe78717c0fa9b
lanakhalifa <lanakhalifa7@gmail.com>
2025-07-24 13:26:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,5 @@ from config.paths_config import *
 from config.training_config import *
 from config.model_configs import *
 
-# Legacy compatibility - map old names to new config system
-DATALOADERS_DIR = str(DATALOADERS_DIR)
-DIR_DATALOADERS = DATALOADERS_DIR
-PATCHES_DIR = str(PATCHES_DIR)
-DATASETS_DIR = str(DATASETS_DIR)
-
 # Ensure all directories exist
 ensure_directories()
